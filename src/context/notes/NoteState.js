c3da5c8e49f18b427b6276850fb25f27178cfd21
unknown <bnerkar63@gmail.com>
2022-04-10 23:39:55 +0530

    V60 - Fetch  data from Context API

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,17 +4,32 @@ import NoteContext from "./noteContext";
 
 
 const NoteState = (props) =>{
+    const notesInitials = [
+        {
+          "_id": "6251c94086ae5a873a1a6a55",
+          "user": "6251a3c10196acdd471ddeb6",
+          "title": "My title",
+          "description": "New notes",
+          "tag": "personal",
+          "date": "2022-04-09T17:58:24.148Z",
+          "__v": 0
+        },
+        {
+          "_id": "6251d80cdad435b9c72e8862",
+          "user": "6251a3c10196acdd471ddeb6",
+          "title": "My title",
+          "description": "New notes",
+          "tag": "personal",
+          "date": "2022-04-09T19:01:32.073Z",
+          "__v": 0
+        }
+      ]
 
-    
-    const s1 ={
-        "name":"bhushan",
-        "class":"5B"
-    }
-   
+      const[notes,setNotes] = useState(notesInitials)
     
     return(
       
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
 
         </NoteContext.Provider>
